d37bcde892a0092bace31daec6cfcd841d5ef896
Cem GEÇGEL <gecgelcem@outlook.com>
2025-08-02 13:44:05 +0300

    Add testing `trk::PlatformError`

--- sources/trkTest/main.cpp
+++ sources/trkTest/main.cpp
@@ -2,15 +2,20 @@
 #include <stdio.h>
 #include <trk/Crashes.hpp>
 #include <trk/Demo.hpp>
+#include <trk/PlatformError.hpp>
 #include <trk/StandardError.hpp>
 
 auto main(int, char **) -> int {
     if (fopen("some/invalid/path", "r") == nullptr) {
         static constexpr auto kCapacity = 1024;
-        auto error = trk::StandardError::getCurrent();
+        auto platformError = trk::PlatformError::getCurrent();
+        auto standardError = trk::StandardError::getCurrent();
         char buffer[kCapacity];
-        if (error.format(buffer, kCapacity) != 0) {
-            (void)fprintf(stderr, "standard error 0x%08" PRIX32 ": %s\n", error.getCode(), buffer);
+        if (platformError.format(buffer, kCapacity) != 0) {
+            (void)fprintf(stderr, "platform error 0x%08" PRIX32 ": %s\n", platformError.getCode(), buffer);
+        }
+        if (standardError.format(buffer, kCapacity) != 0) {
+            (void)fprintf(stderr, "standard error 0x%08" PRIX32 ": %s\n", standardError.getCode(), buffer);
         }
         trk::Crashes::crash();
     }
