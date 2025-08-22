125e413b47f818fb524977919924845b262c8e81
Cem GEÇGEL <gecgelcem@outlook.com>
2025-08-22 13:43:55 +0300

    Refactor `tomurcuk::LinearMemoryAllocator` with `tomurcuk::Result`

--- packages/memory/private/tomurcuk/LinearMemoryAllocator.cpp
+++ packages/memory/private/tomurcuk/LinearMemoryAllocator.cpp
@@ -3,32 +3,39 @@
 #include <tomurcuk/Bytes.hpp>
 #include <tomurcuk/LinearMemoryAllocator.hpp>
 #include <tomurcuk/MemoryAllocator.hpp>
+#include <tomurcuk/Result.hpp>
+#include <tomurcuk/Status.hpp>
 #include <tomurcuk/VirtualBlock.hpp>
 
 static_assert(sizeof(void *) == 8);
 
-auto tomurcuk::LinearMemoryAllocator::initialize(int64_t capacity) -> int64_t {
-    mCursor = 0;
-    return mVirtualBlock.initialize(capacity);
+auto tomurcuk::LinearMemoryAllocator::create(int64_t capacity) -> Result<LinearMemoryAllocator> {
+    auto virtualBlock = VirtualBlock::create(capacity);
+    if (virtualBlock.isFailure()) {
+        return Result<LinearMemoryAllocator>::failure();
+    }
+
+    LinearMemoryAllocator linearMemoryAllocator;
+    linearMemoryAllocator.mVirtualBlock = *virtualBlock.value();
+    linearMemoryAllocator.mCursor = 0;
+    return Result<LinearMemoryAllocator>::success(linearMemoryAllocator);
 }
 
 auto tomurcuk::LinearMemoryAllocator::destroy() -> void {
     mVirtualBlock.destroy();
 }
 
-auto tomurcuk::LinearMemoryAllocator::getMemoryAllocator() -> MemoryAllocator {
-    MemoryAllocator memoryAllocator;
-    memoryAllocator.initialize(this, &reallocateImplementation);
-    return memoryAllocator;
+auto tomurcuk::LinearMemoryAllocator::memoryAllocator() -> MemoryAllocator {
+    return MemoryAllocator::create(this, &reallocateImplementation);
 }
 
-auto tomurcuk::LinearMemoryAllocator::getCursor() -> int64_t {
+auto tomurcuk::LinearMemoryAllocator::cursor() -> int64_t {
     return mCursor;
 }
 
 auto tomurcuk::LinearMemoryAllocator::deallocateDownTo(int64_t cursor) -> void {
     assert(cursor >= 0);
-    assert(cursor <= mVirtualBlock.getLoad());
+    assert(cursor <= mVirtualBlock.load());
 
     mCursor = cursor;
 }
@@ -37,16 +44,15 @@ auto tomurcuk::LinearMemoryAllocator::deallocateAll() -> void {
     mCursor = 0;
 }
 
-auto tomurcuk::LinearMemoryAllocator::releaseUnused() -> int64_t {
-    return mVirtualBlock.release(mVirtualBlock.getLoad() - mCursor);
+auto tomurcuk::LinearMemoryAllocator::releaseUnused() -> Status {
+    return mVirtualBlock.release(mVirtualBlock.load() - mCursor);
 }
 
-auto tomurcuk::LinearMemoryAllocator::reallocateImplementation(void *state, void **newBlock, void *oldBlock, int64_t oldSize, int64_t newSize, int64_t alignment) -> bool {
-    return ((LinearMemoryAllocator *)state)->reallocate(newBlock, oldBlock, oldSize, newSize, alignment);
+auto tomurcuk::LinearMemoryAllocator::reallocateImplementation(void *state, void *oldBlock, int64_t oldSize, int64_t newSize, int64_t alignment) -> Result<void *> {
+    return ((LinearMemoryAllocator *)state)->reallocate(oldBlock, oldSize, newSize, alignment);
 }
 
-auto tomurcuk::LinearMemoryAllocator::reallocate(void **newBlock, void *oldBlock, int64_t oldSize, int64_t newSize, int64_t alignment) -> bool {
-    assert(newBlock != nullptr);
+auto tomurcuk::LinearMemoryAllocator::reallocate(void *oldBlock, int64_t oldSize, int64_t newSize, int64_t alignment) -> Result<void *> {
     assert((oldBlock == nullptr) == (oldSize == 0));
     assert(oldSize >= 0);
     assert(newSize >= 0);
@@ -54,7 +60,7 @@ auto tomurcuk::LinearMemoryAllocator::reallocate(void **newBlock, void *oldBlock
 
 #if !defined(NDEBUG)
     if (oldBlock != nullptr) {
-        auto validBegin = (uint64_t)mVirtualBlock.getAddress();
+        auto validBegin = (uint64_t)mVirtualBlock.address();
         auto validEnd = validBegin + (uint64_t)mCursor;
         auto claimBegin = (uint64_t)oldBlock;
         auto claimEnd = claimBegin + (uint64_t)oldSize;
@@ -70,24 +76,22 @@ auto tomurcuk::LinearMemoryAllocator::reallocate(void **newBlock, void *oldBlock
             mCursor -= oldSize;
         }
 
-        *newBlock = nullptr;
-        return true;
+        return Result<void *>::success(nullptr);
     }
 
     // The operation is a reallocation.
     if (oldSize != 0) {
         // The operation wants to realign.
         if ((uint64_t)oldBlock % (uint64_t)alignment != 0) {
-            *newBlock = allocate(newSize, alignment);
-            if (*newBlock == nullptr) {
-                return false;
+            auto newBlock = allocate(newSize, alignment);
+            if (newBlock.isSuccess()) {
+                if (oldSize < newSize) {
+                    Bytes::copyBlock(*newBlock.value(), oldBlock, oldSize);
+                } else {
+                    Bytes::copyBlock(*newBlock.value(), oldBlock, newSize);
+                }
             }
-            if (oldSize < newSize) {
-                Bytes::copyBlock(*newBlock, oldBlock, oldSize);
-            } else {
-                Bytes::copyBlock(*newBlock, oldBlock, newSize);
-            }
-            return true;
+            return newBlock;
         }
 
         // Try resizing it if this was the last allocation.
@@ -95,49 +99,44 @@ auto tomurcuk::LinearMemoryAllocator::reallocate(void **newBlock, void *oldBlock
             // Reclaim if the operation shrinks the block.
             if (newSize <= oldSize) {
                 mCursor -= oldSize - newSize;
-                *newBlock = oldBlock;
-                return true;
+                return Result<void *>::success(oldBlock);
             }
 
             // Make the block grow in place.
-            if (allocate(newSize - oldSize, 1) == nullptr) {
-                return false;
+            if (allocate(newSize - oldSize, 1).isFailure()) {
+                return Result<void *>::failure();
             }
-            *newBlock = oldBlock;
-            return true;
+            return Result<void *>::success(oldBlock);
         }
 
         // Try shrinking in place and leak the shrunk bytes.
         if (newSize <= oldSize) {
-            *newBlock = oldBlock;
-            return true;
+            return Result<void *>::success(oldBlock);
         }
 
         // Leak the old block totally and just allocate a new one.
-        *newBlock = allocate(newSize, alignment);
-        if (*newBlock == nullptr) {
-            return false;
+        auto newBlock = allocate(newSize, alignment);
+        if (newBlock.isSuccess()) {
+            Bytes::copyBlock(*newBlock.value(), oldBlock, oldSize);
         }
-        Bytes::copyBlock(*newBlock, oldBlock, oldSize);
-        return true;
+        return newBlock;
     }
 
     // The operation is an allocation.
-    *newBlock = allocate(newSize, alignment);
-    return *newBlock != nullptr;
+    return allocate(newSize, alignment);
 }
 
 auto tomurcuk::LinearMemoryAllocator::isLastAllocation(void *block, int64_t size) -> bool {
     if (block == nullptr) {
         return false;
     }
-    auto validEnd = (uint64_t)mVirtualBlock.getAddress() + (uint64_t)mCursor;
+    auto validEnd = (uint64_t)mVirtualBlock.address() + (uint64_t)mCursor;
     auto claimEnd = (uint64_t)block + (uint64_t)size;
     return claimEnd == validEnd;
 }
 
-auto tomurcuk::LinearMemoryAllocator::allocate(int64_t size, int64_t alignment) -> void * {
-    auto validEnd = (uint64_t)mVirtualBlock.getAddress() + (uint64_t)mCursor;
+auto tomurcuk::LinearMemoryAllocator::allocate(int64_t size, int64_t alignment) -> Result<void *> {
+    auto validEnd = (uint64_t)mVirtualBlock.address() + (uint64_t)mCursor;
     auto reminder = validEnd % (uint64_t)alignment;
     auto padding = INT64_C(0);
     if (reminder != 0) {
@@ -148,14 +147,12 @@ auto tomurcuk::LinearMemoryAllocator::allocate(int64_t size, int64_t alignment) 
     assert(padding <= INT64_MAX - size);
 
     auto amount = size + padding;
-    auto space = mVirtualBlock.getLoad() - mCursor;
-    if (space < amount) {
-        auto requiredSpace = amount - space;
-        if (mVirtualBlock.reserve(requiredSpace) < requiredSpace) {
-            return nullptr;
-        }
+    auto space = mVirtualBlock.load() - mCursor;
+    if (space < amount && mVirtualBlock.reserve(amount - space) == Status::eFailure) {
+        return Result<void *>::failure();
     }
-    auto block = (void *)((char *)mVirtualBlock.getAddress() + mCursor + padding);
+
+    auto block = (void *)((char *)mVirtualBlock.address() + mCursor + padding);
     mCursor += amount;
-    return block;
+    return Result<void *>::success(block);
 }
--- packages/memory/public/tomurcuk/LinearMemoryAllocator.hpp
+++ packages/memory/public/tomurcuk/LinearMemoryAllocator.hpp
@@ -2,128 +2,28 @@
 
 #include <stdint.h>
 #include <tomurcuk/MemoryAllocator.hpp>
+#include <tomurcuk/Result.hpp>
+#include <tomurcuk/Status.hpp>
 #include <tomurcuk/VirtualBlock.hpp>
 
 namespace tomurcuk {
-    /**
-     * Memory allocator that divides a single block of bytes with a cursor that
-     * moves forward with each allocation.
-     *
-     * The backing memory is virtual. It is mapped as needed.
-     */
     class LinearMemoryAllocator {
     public:
-        /**
-         * Creates a new allocator with a capacity.
-         *
-         * @see VirtualBlock::initialize
-         *
-         * @param[in] capacity The amount of virtual bytes that will be
-         * allocated.
-         * @return The actually allocated amount of virtual bytes on success. On
-         * failure, `0`.
-         */
-        auto initialize(int64_t capacity) -> int64_t;
-
-        /**
-         * Deallocates all the virtual bytes.
-         */
+        static auto create(int64_t capacity) -> Result<LinearMemoryAllocator>;
         auto destroy() -> void;
-
-        /**
-         * Provides it as a @ref MemoryAllocator.
-         *
-         * @return This allocator as a @ref MemoryAllocator.
-         */
-        auto getMemoryAllocator() -> MemoryAllocator;
-
-        /**
-         * Provides the amount of allocated bytes.
-         *
-         * @return The amount of allocated bytes.
-         */
-        auto getCursor() -> int64_t;
-
-        /**
-         * Deallocates all allocations after a previously queried cursor.
-         *
-         * @param[in] cursor The cursor that marks the first deallocated byte.
-         */
+        auto memoryAllocator() -> MemoryAllocator;
+        auto cursor() -> int64_t;
         auto deallocateDownTo(int64_t cursor) -> void;
-
-        /**
-         * Deallocates all the allocations.
-         */
         auto deallocateAll() -> void;
-
-        /**
-         * Unmaps all unused virtual bytes so that memory usage goes down.
-         *
-         * @return The actually unmapped amount of bytes on success. On failure,
-         * `-1`.
-         */
-        auto releaseUnused() -> int64_t;
+        auto releaseUnused() -> Status;
 
     private:
-        /**
-         * The implementation of @ref MemoryAllocator::mReallocate.
-         *
-         * @param[in,out] state The pointer to the state of the allocator.
-         * @param[out] newBlock The pointer to the newly allocated block.
-         * @param[in] oldBlock The pointer to the block that will be reallocated.
-         * Must be `nullptr` if `oldSize` is `0`.
-         * @param[in] oldSize The amount of bytes in `oldBlock`.
-         * @param[in] newSize The amount of bytes that is desired to be in the
-         * resulting block.
-         * @param[in] alignment The amount of bytes the address must be evenly
-         * divisible to.
-         * @return Whether the request succeeded.
-         */
-        static auto reallocateImplementation(void *state, void **newBlock, void *oldBlock, int64_t oldSize, int64_t newSize, int64_t alignment) -> bool;
-
-        /**
-         * Changes the count of an array of bytes in the allocator.
-         *
-         * @param[out] newBlock The pointer to the newly allocated block.
-         * @param[in] oldBlock The pointer to the block that will be reallocated.
-         * Must be `nullptr` if `oldSize` is `0`.
-         * @param[in] oldSize The amount of bytes in `oldBlock`.
-         * @param[in] newSize The amount of bytes that is desired to be in the
-         * resulting block.
-         * @param[in] alignment The amount of bytes the address must be evenly
-         * divisible to.
-         * @return Whether the request succeeded.
-         */
-        auto reallocate(void **newBlock, void *oldBlock, int64_t oldSize, int64_t newSize, int64_t alignment) -> bool;
-
-        /**
-         * Tests whether the given block's end corresponds to the current
-         * position of the cursor.
-         *
-         * @return Whether the given block's end corresponds to the current
-         * position of the cursor.
-         */
+        static auto reallocateImplementation(void *state, void *oldBlock, int64_t oldSize, int64_t newSize, int64_t alignment) -> Result<void *>;
+        auto reallocate(void *oldBlock, int64_t oldSize, int64_t newSize, int64_t alignment) -> Result<void *>;
         auto isLastAllocation(void *block, int64_t size) -> bool;
+        auto allocate(int64_t size, int64_t alignment) -> Result<void *>;
 
-        /**
-         * Allocates an amount bytes after the cursor.
-         *
-         * @param[in] size The amount of bytes that will be allocated.
-         * @param[in] alignment The amount of bytes the address must be evenly
-         * divisible to.
-         * @return The address of the new allocation on success. On failure,
-         * `nullptr`.
-         */
-        auto allocate(int64_t size, int64_t alignment) -> void *;
-
-        /**
-         * Backing storage.
-         */
         VirtualBlock mVirtualBlock;
-
-        /**
-         * Amount of allocated bytes.
-         */
         int64_t mCursor;
     };
 }
