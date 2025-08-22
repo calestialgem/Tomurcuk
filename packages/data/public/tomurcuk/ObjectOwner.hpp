9767fdae76c94afe68929643abc793918a31b44e
Cem GEÇGEL <gecgelcem@outlook.com>
2025-08-22 13:55:19 +0300

    Add `tomurcuk::ObjectOwner`

---
+++ packages/data/public/tomurcuk/ObjectOwner.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdint.h>
+#include <tomurcuk/MemoryAllocator.hpp>
+#include <tomurcuk/Result.hpp>
+#include <tomurcuk/Status.hpp>
+
+namespace tomurcuk {
+    template<typename Object>
+    class ObjectOwner {
+    public:
+        static auto of(Object *pointer) -> ObjectOwner<Object> {
+            ObjectOwner<Object> objectOwner;
+            objectOwner.mPointer = pointer;
+            return objectOwner;
+        }
+
+        static auto null() -> ObjectOwner<Object> {
+            return of(nullptr);
+        }
+
+        static auto create(MemoryAllocator memoryAllocator) -> Result<ObjectOwner<Object>> {
+            auto blockResult = memoryAllocator.allocate(sizeof(Object), alignof(Object));
+            if (blockResult.isFailure()) {
+                return Result<ObjectOwner<Object>>::failure();
+            }
+
+            return Result<ObjectOwner<Object>>::success(of((Object *)*blockResult.value()));
+        }
+
+        auto destroy(MemoryAllocator memoryAllocator) -> void {
+            memoryAllocator.deallocate(mPointer, sizeof(Object), alignof(Object));
+        }
+
+        auto isNull() -> bool {
+            return mPointer == nullptr;
+        }
+
+        auto get() -> Object * {
+            return mPointer;
+        }
+
+    private:
+        Object *mPointer;
+    };
+}
