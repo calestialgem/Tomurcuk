864ec84068f32d2999c95ec41679e3561665658c
Cem GEÇGEL <gecgelcem@outlook.com>
2025-08-22 16:25:01 +0300

    Add `tomurcuk::ArrayView`

--- packages/data/public/tomurcuk/ArrayView.hpp
+++ packages/data/public/tomurcuk/ArrayView.hpp
@@ -1,13 +1,52 @@
-/*
- * `ArrayView` is a reference to an array that is formed by a `ArrayPointer` and
- * a length.
- *
- * There might not be a referred array if the pointer is `nullptr`. That state
- * should work like an empty array.
- *
- * The referred portion can be shrunk from the beginning and/or the end. The
- * helper functions that provide this return a new `ArrayView` instead of
- * modifying the existing one.
- *
- *
- */
+#pragma once
+
+#include <assert.h>
+#include <stdint.h>
+#include <tomurcuk/ArrayReference.hpp>
+
+namespace tomurcuk {
+    template<typename Element>
+    class ArrayView {
+    public:
+        static auto of(ArrayReference<Element> arrayReference, int64_t length) -> ArrayView {
+            ArrayView arrayView;
+            arrayView.mArrayReference = arrayReference;
+            arrayView.mLength = length;
+            return arrayView;
+        }
+
+        static auto null() -> ArrayView {
+            return of(ArrayReference<Element>::null(), 0);
+        }
+
+        auto isNull() -> bool {
+            return mArrayReference.isNull();
+        }
+
+        auto reference() -> ArrayReference<Element> {
+            return mArrayReference;
+        }
+
+        auto pointer() -> Element * {
+            return mArrayReference.pointer();
+        }
+
+        auto first() -> Element * {
+            return get(0);
+        }
+
+        auto last() -> Element * {
+            return get(mLength - 1);
+        }
+
+        auto get(int64_t index) -> Element * {
+            assert(index < mLength);
+
+            return mArrayReference.get(index);
+        }
+
+    private:
+        ArrayReference<Element> mArrayReference;
+        int64_t mLength;
+    };
+}
